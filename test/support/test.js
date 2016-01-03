7ca51b142726c7b7c21ddc194932d081d5f0ddfe
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2016-01-03 11:31:58 +0800

    Use tape instead of mocha

--- test/support/test.js
+++ test/support/test.js
@@ -1,22 +1,8 @@
-var assert = require('assert')
-
-function test (input, output) {
-  var label = input.replace(/\n/g, '· ')
-  it(label, testRun(input, output))
-}
-
-test.only = function (input, output) {
-  var label = input.replace(/\n/g, '· ')
-  it.only(label, testRun(input, output))
-}
-
-function testRun (input, output) {
-  return function () {
-    if (typeof output === 'object' && output.match) {
-      assert.ok(global.md.render(input).match(output.match))
-    } else {
-      assert.equal(global.md.render(input), output, ' ')
-    }
+function test (md, t, input, output) {
+  if (typeof output === 'object' && output.match) {
+    t.ok(md.render(input).match(output.match), input)
+  } else {
+    t.equal(md.render(input), output, input)
   }
 }
 
